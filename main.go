ae1005356937d1478412e91f4b96d019b675d447
Mikael Escolin <mikaelescolin@gmail.com>
2025-10-27 11:46:54 +0000

    Update main.go
    Been so long since I changed this. I think it had changes with using my
    own env reader module, updating where the folder gets created by using
    the env file, and sanitizing file names.

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path"
-	// "path/filepath"
-	// "strings"
+	"path/filepath"
+	"strconv"
+
+	"github.com/mikogd/hextech/env"
 )
 
 func changeDirectory(path string) {
@@ -17,47 +18,84 @@ func changeDirectory(path string) {
 }
 
 func start() uint8 {
+	env.LoadEnv("./.env")
+
+	metadata, err := GetMetaDataForCD()
+	if err != nil {
+		log.Printf("Failed to get meta data: %v\n", err)
+		return 1
+	}
+
+	release := GetRelease(metadata)
+
+	pathToMusicFolder := os.Getenv("PATH_TO_DEST_MUSIC")
+	if pathToMusicFolder == "" {
+		log.Fatalf("Failed to get PATH_TO_DEST_MUSIC environment variable")
+	}
+
+	artistName := release.AristCredit.NameCredit[0].Artist.Name
+	albumName := release.Title
+	pathToAlbum := path.Join(pathToMusicFolder, artistName, sanitizeSongName(albumName))
+	if _, err := os.Stat(pathToAlbum); os.IsNotExist(err) {
+		log.Printf("%s doesn't exist, creating directory\n", pathToAlbum)
+		if err = os.MkdirAll(pathToAlbum, 0777); err != nil {
+			log.Printf("Failed to create directory %s: %s\n", pathToAlbum, err)
+			return 1
+		}
+	} else if err != nil {
+		// Some other error while trying to check the folder
+		log.Printf("Error checking directory %s: %s\n", pathToAlbum, err)
+		return 1
+	}
+
 	startingWorkingDirectory, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Failed to get the current working directory: %s\n", err)
 	}
 
-	tempDirPath, err := os.MkdirTemp("", "cd-rip")
-	if err != nil {
-		log.Fatalf("Failed to make temporary directory: %s\n", err)
-	}
-	defer os.RemoveAll(tempDirPath)
-
-	log.Printf("Created temp directory %s\n", tempDirPath)
-
-	if err = os.Chdir(tempDirPath); err != nil {
-		log.Printf("Failed to do change current working directory to %s\n", tempDirPath)
+	if err = os.Chdir(pathToAlbum); err != nil {
+		log.Printf("Failed to do change current working directory to %s\n", pathToAlbum)
 		log.Printf("Aborting...")
 		return 1
 	}
 
 	defer changeDirectory(startingWorkingDirectory)
 
-	metadata, err := GetMetaDataForCD()
-  if err != nil {
-    log.Printf("Failed to get meta data: %v\n", err)
-    changeDirectory(startingWorkingDirectory)
-    return 1
-  }
-
-	if err := RipCD(tempDirPath); err != nil {
+	if err := RipCD(pathToAlbum); err != nil {
 		log.Printf("Failed to rip CD: %s\n", err)
 		changeDirectory(startingWorkingDirectory)
 		os.Exit(1)
 	}
 
-	songs := GetFlacTags(metadata)
+	discNumberArg := ""
+	if len(os.Args) == 2 {
+		discNumberArg = os.Args[1]
+	}
 
-	for song, tags := range songs {
-		trackName := fmt.Sprintf("track%02d.flac", tags.TrackNumber)
-		oldPath := path.Join(tempDirPath, trackName)
-		newFileName := fmt.Sprintf("%s-no-tags.flac", song)
-		newPath := path.Join(tempDirPath, newFileName)
+	var discNumber int
+	if discNumberArg == "" {
+		discNumber = 1
+	} else {
+		discNumber, err = strconv.Atoi(discNumberArg)
+		if err != nil {
+			log.Fatalf("Failed to convert discNumberArg '%s' to int", discNumberArg)
+		}
+	}
+
+	log.Printf("discNumber: %d\n", discNumber)
+
+	songs := GetFlacTags(metadata, uint8(discNumber))
+
+	for _, song := range songs {
+		log.Printf("song: %s\ntrack number: %d\ntags: %v\n\n", song.Title, song.TrackNumber,  song)
+	}
+
+	for _, song := range songs {
+		trackName := fmt.Sprintf("track%02d.flac", song.TrackNumber)
+		oldPath := path.Join(pathToAlbum, trackName)
+
+		newFileName := fmt.Sprintf("%s-no-tags.flac", sanitizeSongName(song.Title))
+		newPath := path.Join(pathToAlbum, newFileName)
 		if err := os.Rename(oldPath, newPath); err != nil {
 			log.Printf("Failed to rename %s to %s: %s\n", oldPath, newPath, err)
 			changeDirectory(startingWorkingDirectory)
@@ -65,44 +103,22 @@ func start() uint8 {
 		}
 	}
 
-	AddFLACTags(songs)
+	AddFLACTags(songs, metadata, uint8(discNumber))
 
-	artistName := metadata.Disc.Releases.Release[0].AristCredit.NameCredit[0].Artist.Name
-	albumName := metadata.Disc.Releases.Release[0].Title
-	pathToAlbum := path.Join("/Volumes", "Cloud 1", "music", artistName, albumName)
-	if err = os.MkdirAll(pathToAlbum, 777); err != nil {
-		log.Printf("Failed to create directory %s: %s\n", pathToAlbum, err)
-		changeDirectory(startingWorkingDirectory)
-		return 1
+	log.Printf("Deleting all *.cdda.wav files")
+	matches, err := filepath.Glob("*.cdda.wav")
+	for _, match := range matches {
+		if err := os.Remove(match); err != nil {
+			log.Printf("Failed to remove %s\n", match)
+		}
 	}
-	for song, tags := range songs {
-		fileName := fmt.Sprintf("%d. %s.flac", tags.TrackNumber, song)
-		oldPath := path.Join(tempDirPath, fileName)
-		newPath := path.Join(pathToAlbum, fileName)
 
-		songFile, err := os.Open(fileName)
-		if err != nil {
-			log.Printf("Failed to open file %s: %s\n", oldPath, err)
-			changeDirectory(startingWorkingDirectory)
-			return 1
+	log.Printf("Deleting all *-no-tags.flac files")
+	matches, err = filepath.Glob("*-no-tags.flac")
+	for _, match := range matches {
+		if err := os.Remove(match); err != nil {
+			log.Printf("Failed to remove %s\n", match)
 		}
-
-		destSongFile, err := os.Create(newPath)
-		if err != nil {
-			log.Printf("Failed to create dest song file %s: %s\n", newPath, err)
-			changeDirectory(startingWorkingDirectory)
-			return 1
-		}
-
-		_, err = io.Copy(destSongFile, songFile)
-		if err != nil {
-			log.Printf("Failed to write song %s to %s: %s\n", oldPath, newPath, err)
-			changeDirectory(startingWorkingDirectory)
-			return 1
-		}
-
-		songFile.Close()
-		destSongFile.Close()
 	}
 
 	return 0
